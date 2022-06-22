094f245c1ede9ccb54b3994a665b8062b3eddb4a
Shan <shantanu@zantrik.in>
2022-06-23 01:18:27 +0530

    Bg Changes

--- src/components/ImageOne.js
+++ src/components/ImageOne.js
@@ -1,5 +1,5 @@
 import { Parallax } from 'react-parallax';
-import landingimg from '../img/landingpage.jpeg'
+import landingimg from '../img/alianbg_op.png'
 const ImageOne = () => (
     <Parallax className='image' blur={0} bgImage={landingimg} strength={600} bgImageStyle={{ minHeight: "100vh" }}>
         <div className='content'>
--- src/components/ImageTwo.js
+++ src/components/ImageTwo.js
@@ -1,10 +1,12 @@
 import { Parallax } from 'react-parallax';
-import Porsche from '../img/porsche.jpeg'
+import Porsche from '../img/Background.png'
+import Man from '../img/man.png'
 
 const ImageTwo = () => (
     <Parallax className='image' blur={0} bgImage={Porsche} strength={600} bgImageStyle={{ minHeight: "100vh" }}>
         <div className='content'>
             <span className="img-txt">MAD WORLD</span>
+            <img src={Man} />
         </div>
     </Parallax>
 );
