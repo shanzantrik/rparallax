7b357a0a07a38353d26069eddf68f0847f27e227
Shan <shantanu@zantrik.in>
2022-06-23 01:29:31 +0530

    Change Man to landing

--- src/components/ImageOne.js
+++ src/components/ImageOne.js
@@ -1,9 +1,15 @@
 import { Parallax } from 'react-parallax';
 import landingimg from '../img/alianbg_op.png'
+import Man from '../img/man.png'
+
+
 const ImageOne = () => (
     <Parallax className='image' blur={0} bgImage={landingimg} strength={600} bgImageStyle={{ minHeight: "100vh" }}>
         <div className='content'>
             <span className="img-txt">MAD WORLD</span>
+            <Parallax translateY={[-20, 20]}>
+                <img src={Man} alt="Man" />
+            </Parallax>
         </div>
     </Parallax>
 );
--- src/components/ImageTwo.js
+++ src/components/ImageTwo.js
@@ -1,12 +1,11 @@
 import { Parallax } from 'react-parallax';
 import Porsche from '../img/Background.png'
-import Man from '../img/man.png'
+
 
 const ImageTwo = () => (
     <Parallax className='image' blur={0} bgImage={Porsche} strength={600} bgImageStyle={{ minHeight: "100vh" }}>
         <div className='content'>
             <span className="img-txt">MAD WORLD</span>
-            <img src={Man} />
         </div>
     </Parallax>
 );
